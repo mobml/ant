374899100c2ce850319906777f16662c2e635fcc
Oscar Murrieta <oscarmlmurrieta@gmail.com>
2025-12-05 16:16:10 -0600

    chore: add plan_repository

---
+++ internal/repositories/plan_repository.go
@@ -0,0 +1,147 @@
+package repositories
+
+import (
+	"database/sql"
+	"fmt"
+	"github.com/google/uuid"
+	"github.com/mobml/ant/internal/models"
+)
+
+type PlanRepository interface {
+	Create(plan *models.Plan) error
+	List() ([]*models.Plan, error)
+	FindByID(id uuid.UUID) (*models.Plan, error)
+	Update(p *models.Plan) error
+	Delete(id uuid.UUID) error
+}
+
+type planRepository struct {
+	db *sql.DB
+}
+
+func NewPlanRepository(db *sql.DB) PlanRepository {
+	return &planRepository{db: db}
+}
+
+func (r *planRepository) Create(p *models.Plan) error {
+	query := `
+		INSERT INTO plans (id, name, description, start_date, duration, created_at, updated_at)
+		VALUES (?, ?, ?, ?, ?, ?, ?)
+	`
+	_, err := r.db.Exec(
+		query,
+		p.ID,
+		p.Name,
+		p.Description,
+		p.StartDate,
+		p.Duration,
+		p.CreatedAt,
+		p.UpdatedAt,
+	)
+
+	if err != nil {
+		return fmt.Errorf("failed to create plan: %w", err)
+	}
+	return nil
+}
+
+func (r *planRepository) List() ([]*models.Plan, error) {
+	query := "SELECT * FROM plans;"
+
+	rows, err := r.db.Query(query)
+
+	if err != nil {
+		return nil, fmt.Errorf("failed to list plans: %w", err)
+	}
+	defer rows.Close()
+
+	var plans []*models.Plan
+
+	for rows.Next() {
+		var p models.Plan
+
+		if err := rows.Scan(
+			&p.ID,
+			&p.Name,
+			&p.Description,
+			&p.StartDate,
+			&p.Duration,
+			&p.CreatedAt,
+			&p.UpdatedAt,
+		); err != nil {
+			return nil, fmt.Errorf("failed to reads rows: %w", err)
+		}
+		plans = append(plans, &p)
+	}
+
+	if err := rows.Err(); err != nil {
+		return nil, fmt.Errorf("failed during the rows interations: %w", err)
+	}
+
+	return plans, nil
+}
+
+func (r *planRepository) FindByID(id uuid.UUID) (*models.Plan, error) {
+	query := "SELECT * from plans WHERE id = ?"
+
+	row := r.db.QueryRow(query, id)
+
+	var p models.Plan
+
+	if err := row.Scan(
+		&p.ID,
+		&p.Name,
+		&p.Description,
+		&p.StartDate,
+		&p.Duration,
+		&p.CreatedAt,
+		&p.UpdatedAt,
+	); err != nil {
+		if err == sql.ErrNoRows {
+			return nil, nil
+		}
+
+		return nil, fmt.Errorf("failed to find plan: %w", err)
+	}
+
+	if err := row.Err(); err != nil {
+		return nil, fmt.Errorf("failed during the row interations: %w", err)
+	}
+
+	return &p, nil
+}
+
+func (r *planRepository) Update(p *models.Plan) error {
+	query := `
+		UPDATE plans 
+		SET name = ?, description = ?, duration = ?, updated_at = ? 
+		WHERE id = ?
+	`
+
+	_, err := r.db.Exec(
+		query,
+		p.Name,
+		p.Description,
+		p.StartDate,
+		p.Duration,
+		p.UpdatedAt,
+		p.ID,
+	)
+
+	if err != nil {
+		return fmt.Errorf("failed updating plan '%s': %w", p.ID, err)
+	}
+	return nil
+}
+
+func (r *planRepository) Delete(id uuid.UUID) error {
+	query := `DELETE FROM plans WHERE id = ?`
+
+	_, err := r.db.Exec(query, id)
+
+	if err != nil {
+		return fmt.Errorf("failed deleting plan '%s': %w", id, err)
+	}
+
+	return nil
+}
